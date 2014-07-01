00e1893a6fb21be335f9f19628fa492cdc739d4e
Patricia Lipp <patricia.lipp@immobilienscout24.de>
2014-07-01 10:13:19 +0200

    more...

---
+++ src/main/java/java8/libraries_3/OptimizationsForPrimitives_1.java
@@ -0,0 +1,24 @@
+package java8.libraries_3;
+
+import java8.testdata_0.Album;
+import java8.testdata_0.SampleData;
+
+import java.util.IntSummaryStatistics;
+
+public class OptimizationsForPrimitives_1 {
+
+  public static void main(String[] args) {
+    Album album = SampleData.manyTrackAlbum;
+   
+    IntSummaryStatistics trackLengthStats
+      = album.getTracks()
+      .mapToInt(track -> track.getLength()) // calculates with int (not Integer)
+      .summaryStatistics(); // convenience methods for ints
+
+    System.out.printf("Max: %d, Min: %d, Ave: %f, Sum: %d",
+      trackLengthStats.getMax(),
+      trackLengthStats.getMin(),
+      trackLengthStats.getAverage(),
+      trackLengthStats.getSum());
+  }
+}
