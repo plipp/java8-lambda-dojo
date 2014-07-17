dd2422e41e4957cd802a0315f6e0f59c628b26bc
plipp <patricia.lipp@immobilienscout24.de>
2014-07-17 22:39:15 +0200

    next chapter

---
+++ src/main/java/java8/advanced_collections_4/AdvancedCollectors_1.java
@@ -0,0 +1,19 @@
+package java8.advanced_collections_4;
+
+import java8.testdata_0.Album;
+import java8.testdata_0.Artist;
+import java8.testdata_0.SampleData;
+
+import java.util.OptionalDouble;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class AdvancedCollectors_1 {
+    public static void main(String[] args) {
+        OptionalDouble average1 = SampleData.albums.mapToInt(album -> album.getTrackList().size()).average();
+        System.out.println(average1);
+
+//        Double average2 = SampleData.albums.collect(Collectors.averagingInt(album -> album.getTrackList().size()));
+//        System.out.println(average2);
+    }
+}
