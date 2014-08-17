1937c493e007eecbbda2ebbb634bb68c9be8bf08
plipp <patricia.lipp@immobilienscout24.de>
2014-08-17 20:44:54 +0200

    advanced collections

--- src/main/java/java8/advanced_collections_4/exercises/Fibonacci_3.java
+++ src/main/java/java8/advanced_collections_4/exercises/Fibonacci_3.java
@@ -5,6 +5,9 @@ import java8.exercises_0.Exercises;
 import java.util.HashMap;
 import java.util.Map;
 
+// Efficiently calculate a Fibonacci sequence using just the computeIfAbsent method
+//  on a Map. By “efficiently,” I mean that you don’t repeatedly recalculate the Fibonacci
+//  sequence of smaller numbers.
 public class Fibonacci_3 {
 
     public Fibonacci_3() {
--- src/main/java/java8/advanced_collections_4/exercises/GroupingBy_2_3.java
+++ src/main/java/java8/advanced_collections_4/exercises/GroupingBy_2_3.java
@@ -9,6 +9,19 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collector;
 
+// TODO: Not now!
+// Implement Collectors.groupingBy as a custom collector.
+//
+// You don’t need to provide a downstream collector, so just implementing the simplest variant is
+// fine.
+//
+// If you look at the JDK source code, you’re cheating!
+//
+// Hint:
+// You might want to start with
+//     public class GroupingBy<T, K> implements Collector<T, Map<K, List<T>>, Map<K, List<T>>>.
+// This is an advanced exercise, so you might want to attempt it last.
+
 public class GroupingBy_2_3<T, K> implements Collector<T, Map<K, List<T>>, Map<K, List<T>>> {
 
     private final Function<? super T, ? extends K> classifier;
