61628235f12a85bf38c0d60f9a767a0d49b2f5c5
plipp <patricia.lipp@immobilienscout24.de>
2015-01-18 06:31:50 +0100

    fix documentation

--- src/main/java/java8/euler_99/Problems.java
+++ src/main/java/java8/euler_99/Problems.java
@@ -34,10 +34,10 @@ public class Problems {
   /**
    * The sum of the squares of the first ten natural numbers is,
    *
-   * 12 + 22 + ... + 102 = 385
+   * 1^2 + 2^2 + ... + 10^2 = 385
    * The square of the sum of the first ten natural numbers is,
    *
-   * (1 + 2 + ... + 10)2 = 552 = 3025
+   * (1 + 2 + ... + 10)^2 = 55^2 = 3025
    * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
    *
    * @return the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
@@ -48,7 +48,7 @@ public class Problems {
   }
 
   /**
-   * 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+   * 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
    *
    * @return the sum of the digits of the number 2^1000
    * @see <a href="https://projecteuler.net/problem=16">Problem16</a>
